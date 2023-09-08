5e4b162f93e643ed9375718a47293a74ecd3c280
Tesvin <stephenazosike@gmail.com>
2023-09-08 20:59:35 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.json({title: 'hello world'})
+}) 
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000')
+})
