770f7e7625b75350485b862ce755fb223030587c
Tesvin <stephenazosike@gmail.com>
2023-09-09 17:50:25 +0100

    HNGi

--- server.js
+++ server.js
@@ -1,11 +1,63 @@
 const express = require('express');
+const cors = require('cors');
+const jsonc = require('jsonc')
 const app = express();
+//const dateFunction = require('./controller.js')
+
+require('dotenv').config();
+const port = process.env.Port || 3000
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors())
 
 
-app.get('/', (req, res) => {
-    res.json({title: 'hello world'})
+    //get current day 
+    day = new Date().getUTCDay()
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const todays_day = daysOfWeek[day]
+
+    // utc time
+    const now = new Date();
+    // const offsetMinutes = 2;
+
+    // Calculate the adjusted time by adding or subtracting minutes
+    now.setMinutes(now.getUTCMinutes());
+
+    // Format the date in "yyyy-MM-ddTHH:mm:ssZ" format
+    const todays_time = now.toISOString().slice(0, 19) + "Z";
+
+
+
+// Endpoint
+app.get('/api', (req, res) => {
+
+    // get parameters
+    const slack_name = req.query.slack_name;
+    const track = req.query.track
+
+    // Status code 
+    const statusCode = res.statusCode;
+
+
+
+
+    // Creat JSON response
+    const responseObject = {
+        "slack_name": slack_name,
+        "current_day": todays_day,
+        "utc_time": todays_time,
+        "track": track,
+        "github_file_url": "https://github.com/Tesvin/Create-and-host-and-endpoint-HNGi/blob/main/server.js",
+        "github_repo_url": "https://github.com/Tesvin/Create-and-host-and-endpoint-HNGi",
+        "status_code": statusCode
+    }
+
+    res.send(responseObject)
+   
 }) 
 
-app.listen(3000, () => {
-    console.log('App listening on port 3000')
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`)
 })
